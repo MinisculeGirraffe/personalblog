c6485f2d0a1fc673dd3f6f0877308e222c6c5ad3
Daniel Norred <daniel@nor.red>
2021-01-19 07:21:27 -0600

    Update renderer

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,13 @@
 import ReactMarkdown from "react-markdown"
 import {CodeBlock} from './codeblock'
+const gfm = require('remark-gfm')
+
 export default function PostBody({ content }) {
   console.log(content)
   return (
     <div className="max-w-2xl mx-auto">
       <ReactMarkdown
+        plugins={[gfm]}
         children={content}
         escapeHtml={false}
         renderers={{code: CodeBlock}}
