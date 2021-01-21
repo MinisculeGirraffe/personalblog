55af90056077cf0f384d512d5f99ff3ae1dcce4a
Daniel Norred <daniel@nor.red>
2021-01-21 13:47:49 -0600

    Update extensions

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,9 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
-import Prism from "Prismjs";
-import "prismjs/components/prism-jsx.min";
-import "prismjs/components/prism-powershell.min"
+import prism from "Prismjs";
+import "prismjs/components/prism-jsx.min.js";
+import "prismjs/components/prism-powershell.min.js"
 import {useEffect} from 'react'
 
 export default function Layout({ preview, children }) {
