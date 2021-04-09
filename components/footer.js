c2e6cc6385548ad5d1ba12b85d48a04c67daa22e
Daniel Norred <daniel@nor.red>
2021-04-09 13:37:06 -0500

    Add footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="bg-color-basic-900  text-white max-h-4">
-      <p>Daniel Norred</p>
+    <footer className="bg-color-basic-900  text-white max-h-4 flex-row	p-r-5	">
+      <div className=' flex justify-between'>
+      <p>Daniel Norred - daniel@nor.red</p>
+      <p>AAAAC3NzaC1lZDI1NTE5AAAAIJOeCgoGbVNp2bxYAQbS5+g1SgiiH8TGByCZoQNorred</p>
+      </div>
+
     </footer>
   )
 }
--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Intro from '../components/intro'
 import prism from 'prismjs'
 import "prismjs/components/prism-jsx.min.js";
 import "prismjs/components/prism-powershell.min.js"
-import {initGA, logPageView} from '../lib/analytics'
+import { initGA, logPageView } from '../lib/analytics'
 import { useEffect } from 'react'
 
 export default function Layout({ preview, children }) {
@@ -24,7 +24,6 @@ export default function Layout({ preview, children }) {
         <div className='flex-1 flex-wrap min-h-screen mt-20 w-screen'>
           <main>{children}</main>
         </div>
-        
       </div>
       <Footer />
     </>
